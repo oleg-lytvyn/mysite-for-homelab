a5b7d531bbf3f5a7870855df94e0703bc49a0da6
oleh <olehlytvyn.personal@gmail.com>
2024-11-11 12:52:36 +0000

    Modifying the views/post_detail. Modifying the canonical URL for posts/models.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
 from . import views
-
 app_name = 'blog'
-
 urlpatterns = [
+    # Post views
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    # path('<int:id>/', views.post_detail, name='post_detail'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'
+    )
 ]
